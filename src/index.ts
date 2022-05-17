522a94f9f43fc05dacf9856e44189b93a7378ef7
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-05-16 21:53:53 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let v1: number[] = new Array(6);
+let v2: number[] = new Array(6);
+let sumav: number[] = new Array(6);
+let indice: number;
+
+for (indice = 0; indice < 6; indice++) {
+  v1[indice] = Number(prompt("Ingrese el valor v1 [" + indice + "]"));
+
+  cos;
+}
